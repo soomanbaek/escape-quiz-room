16c8de87ae8d77f42ebb047a18464a7353ccd918
soomanbaek <qortnaks311@gmail.com>
2026-05-25 17:17:25 +0900

    방탈출 퀴즈 게임

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   viewportFit: 'cover',
+  interactiveWidget: 'resizes-visual',
 }
 
 export const metadata: Metadata = {
